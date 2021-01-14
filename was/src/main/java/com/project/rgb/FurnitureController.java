d3be9949520ab3f7d367491030765676af4f5c72
신현정 <jsi06138@gmail.com>
2021-01-14 23:50:32 +0900

    Update FurnitureController.java
    - 오류발생으로 가구 관련 코드 변경

--- was/src/main/java/com/project/rgb/FurnitureController.java
+++ was/src/main/java/com/project/rgb/FurnitureController.java
@@ -44,6 +44,7 @@ public class FurnitureController {
 			model.addAttribute("proLen", list.size());
 			return "/furniture/FurnitureListView";
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
@@ -55,12 +56,18 @@ public class FurnitureController {
 			Object[] obj = furnitureService.get(id);
 			ProductDTO productDTO = (ProductDTO) obj[0];
 			ArrayList<ImageDTO> imageList = (ArrayList<ImageDTO>) obj[1];
+			// System.out.println(((ProductDTO) obj[0]).getName());
+			
+			System.out.println("컨트롤러" + productDTO.getParentCategory() + " " + productDTO.getChildCategory());
 			
 			model.addAttribute("product", productDTO);
 			model.addAttribute("imageList", imageList);
-		
+			for (int i = 0; i < imageList.size(); i++) {
+				System.out.println("경로 " + imageList.get(i).getPath());
+			}
 			return "/furniture/FurnitureView";
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
@@ -97,11 +104,12 @@ public class FurnitureController {
 			model.addAttribute("proLen", list.size());
 			model.addAttribute("list", list);
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		return "/furniture/FurnitureListView";
-	}
+	} // 아니 근데 색상이나 필터는 다른 컬럼에 적용되는데 어떡하지???
 
 	@RequestMapping(value = "/insertArrangeInfo", method = RequestMethod.POST)
 	public String getArrangeInfoPage(@ModelAttribute("productId") int productId,
@@ -109,6 +117,45 @@ public class FurnitureController {
 		model.addAttribute("productId", productId);
 		model.addAttribute("productName", productName);
 
+		System.out.println("상품 아이디" + productId);
+
 		return "/furniture/insertInfo";
 	}
+
+//	@RequestMapping(value = "/arrange", method = RequestMethod.POST)
+//	public String arrange(MultipartFile file, @ModelAttribute(value = "productId") int productId,
+//			@ModelAttribute(value = "productName") String productName,
+//			@ModelAttribute(value = "widthSize") int widthSize, 
+//			@ModelAttribute(value = "heightSize") int heightSize, Model model) {
+//		try {
+//			//BufferedImage image = ImageIO.read(file.getInputStream());
+//			//System.out.print(image.getWidth() + " " + image.getHeight());
+//			
+//			Object[] obj = furnitureService.arrange(productId);
+//			ProductDTO productInfo = (ProductDTO) obj[0];
+//			List<ImageDTO> imageList = (List<ImageDTO>) obj[1];
+//
+//			//String myRoomPath = uploadFile(file.getOriginalFilename(), file.getBytes());
+//			
+//			String myRoomPath = "/resources/image_room/room2.PNG";
+//			model.addAttribute("myRoomPath", myRoomPath);
+//			model.addAttribute("imageList", imageList);
+//			model.addAttribute("productInfo", productInfo);
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} 
+//
+//
+//		return "/furniture/arrange";
+//	}
+//
+//	public String uploadFile(String originalName, byte[] fileData) throws IOException {
+//		UUID uid = UUID.randomUUID();
+//		String savedName = uid.toString() + "_" + originalName; // 얘네 두 줄을 가져다가 컨트롤러로 둬야겠지 진짜 등록까지 하려면???
+//
+//		String folderPath = "/resources/image_room";
+//		File target = new File(defaultPath + folderPath, savedName);
+//		FileCopyUtils.copy(fileData, target); // 이 과정으로 인해 파일저장이 이루어지는 듯
+//		return folderPath + "/" + savedName;
+//	}
 }
